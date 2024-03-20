2d6a12136739a1879c79587b5d81438367127089
onenyeong <a48142826@gmail.com>
2024-03-20 12:09:50 +0900

    :sparkles: feat(User) : UserRepository 생성 및 구현

---
+++ src/main/java/com/fis/mylittleboard/domain/user/repository/UserJpaRepository.java
@@ -0,0 +1,11 @@
+package com.fis.mylittleboard.domain.user.repository;
+
+import com.fis.mylittleboard.domain.user.entity.UserEntity;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserJpaRepository extends JpaRepository<UserEntity,Long> {
+  Optional<UserEntity> findBySignupId(String signupId);
+}
--- src/main/java/com/fis/mylittleboard/domain/user/repository/UserRepository.java
+++ src/main/java/com/fis/mylittleboard/domain/user/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.fis.mylittleboard.domain.user.repository;
 
-import com.fis.mylittleboard.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.fis.mylittleboard.domain.user.dto.SignupRequestDto;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository {
 
+  boolean checksignupId(String signupId);
+
+  void signIn(SignupRequestDto signupRequestDto);
 }

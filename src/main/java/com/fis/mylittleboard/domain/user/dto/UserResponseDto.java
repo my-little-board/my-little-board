c9299273f5501310665dff4bb6c45d76070e9592
onenyeong <a48142826@gmail.com>
2024-03-20 12:07:12 +0900

    :sparkles: feat(User) : dto 생성

--- src/main/java/com/fis/mylittleboard/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/fis/mylittleboard/domain/user/dto/UserResponseDto.java
@@ -1,21 +1,21 @@
 package com.fis.mylittleboard.domain.user.dto;
 
-import com.fis.mylittleboard.domain.user.entity.User;
 import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class UserResponseDto {
-  private long id;
   private String signupId;
-  private String email;
   private String username;
   private LocalDateTime createdAt;
-  public UserResponseDto(User user) {
-    this.id = user.getId();
-    this.signupId = user.getSignupId();
-    this.email = user.getEmail();
-    this.username = user.getUsername();
-    this.createdAt = user.getCreatedAt();
+  private LocalDateTime modifiedAt;
+
+  public UserResponseDto(String signupId,String username,LocalDateTime createdAt, LocalDateTime modifiedAt) {
+    this.signupId = signupId;
+    this.username = username;
+    this.createdAt = LocalDateTime.now();
+    this.modifiedAt = LocalDateTime.now();
   }
 }

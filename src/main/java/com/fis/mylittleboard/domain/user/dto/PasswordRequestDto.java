e028df0737fd150d1fb7235dc898ac7921ed2853
onenyeong <a48142826@gmail.com>
2024-03-22 15:50:33 +0900

    :sparkles: feat(User) : 패스워드 dto 추가

---
+++ src/main/java/com/fis/mylittleboard/domain/user/dto/PasswordRequestDto.java
@@ -0,0 +1,10 @@
+package com.fis.mylittleboard.domain.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PasswordRequestDto {
+  private String password;
+  private String newPassword;
+
+}

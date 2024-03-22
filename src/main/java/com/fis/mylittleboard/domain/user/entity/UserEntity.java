d355772729660a790afd627971d4fe18ca392f8d
onenyeong <a48142826@gmail.com>
2024-03-22 15:51:46 +0900

    :sparkles: feat(User) : 기존패스워드 -> new password 기능

--- src/main/java/com/fis/mylittleboard/domain/user/entity/UserEntity.java
+++ src/main/java/com/fis/mylittleboard/domain/user/entity/UserEntity.java
@@ -1,27 +1,27 @@
 package com.fis.mylittleboard.domain.user.entity;
 
 import com.fis.mylittleboard.domain.user.model.User;
+import com.fis.mylittleboard.global.common.TimeStamp;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 @Entity
 @Getter
 @RequiredArgsConstructor
 @AllArgsConstructor
+
 @Builder
 @Table(name = "users")
-public class UserEntity {
+public class UserEntity extends TimeStamp {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,13 +39,6 @@ public class UserEntity {
   @Column(nullable = false, length = 50)
   private String username;
 
-  @Column(nullable = false)
-  @CreatedDate
-  private LocalDateTime createdAt;
-
-  @Column
-  @LastModifiedDate
-  private LocalDateTime modifiedAt;
 
   public static UserEntity of(String signupId, String password, String email, String username) {
     return UserEntity.builder()
@@ -53,8 +46,6 @@ public class UserEntity {
         .password(password)
         .email(email)
         .username(username)
-        .createdAt(LocalDateTime.now())
-        .modifiedAt(LocalDateTime.now())
         .build();
   }
 
@@ -66,6 +57,9 @@ public class UserEntity {
         .email(email)
         .username(username)
         .build();
+  }
+  public void changePassword(String newPassword) {
+    this.password = newPassword;
   }
 }
 

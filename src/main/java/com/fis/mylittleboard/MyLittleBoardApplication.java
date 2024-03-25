68388bc8b0bd1dbc7849759137764bb81b9110ef
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-03-25 10:46:46 +0900

    :recycle: @ComponentScan 추가

--- src/main/java/com/fis/mylittleboard/MyLittleBoardApplication.java
+++ src/main/java/com/fis/mylittleboard/MyLittleBoardApplication.java
@@ -2,8 +2,10 @@ package com.fis.mylittleboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@ComponentScan(basePackages = {"com.fis.mylittleboard.global.jwt"})
 @SpringBootApplication
 @EnableJpaAuditing
 public class MyLittleBoardApplication {
